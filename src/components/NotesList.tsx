7c9311705e821d6e8c2ec337798ed619ec6712d9
rockymx <2dcommx02@gmail.com>
2025-08-30 23:42:37 -0600

    Fix duplicate import in NotesList.tsx

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,5 +1,4 @@
 import { Note, Folder as FolderType } from '../types';
-import { Note, Folder as FolderType } from '../types';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { MarkdownRenderer } from './MarkdownRenderer';
